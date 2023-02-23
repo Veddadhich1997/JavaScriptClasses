2cab3058fd4559d2aaf0acbda67b71298cb65cc3
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-23 17:12:03 +0530

    Content of first class

---
+++ script.js
@@ -0,0 +1,105 @@
+// History of JavaScript
+// 1995 brendan eich(developer at netscape) 10 days
+// 1996-livescript>>JavaScript
+// InternetExplorer(Miscrosoft browser) 1997>>Jsript
+// ECMA ECMA+JavaScript>>EcmaScript( ECMA was born in 1996)
+// 1997-ES1
+// 2009-ES5
+// 2015-ES6
+// Tc-39 annual release after ES6
+
+// Programming - Where your program is able to make decisions according to the provided conditions
+// Basics of programming
+// 1. values    2. variables    3. operations   4. conditions   5. loops    6. functions
+
+// 1. values
+// 1,1.01,ved
+
+// 2. variables
+        // variables are like a container,simply a name of storage location
+        // 1. local 2. global
+
+//  VedDadhich
+// Ved_Dadhich
+// ved-Dadhich
+// var let const
+// Var-scope of var is global means it can be access anywhere in the program,values can be assigned again
+// let-local scope,values can be assigned
+// const-local scope,values can't be assigned
+
+// 3. Operatios
+// Arithmetic   logical     comparision     assignment      increment/decrement
+// Arithmetic-+ - * / % ++ --
+// var a = 10;
+// var b =5;
+// console.log(a+b);
+
+
+// logical && || !
+// var a = true;
+// var b = true;
+// if(a && b){
+//     console.log("Hello ved");
+// }
+// else{
+//     console.log("Go Back");
+// }
+
+
+// comparision == >= <=  > < ===
+    // var a = 10;
+    // var b = 20;
+    // if(a<=b){
+    //     console.log("Hello Ved Dadhich");
+    // }
+    // else{
+    //     console.log("It is false");
+    // }
+
+
+
+// assignment   = += -= *= /=
+    // var a = 10;
+    // var b = 5;
+    // // a = a+b;
+    // a += b;
+    // console.log(a);
+
+
+// increment-decrement
+// var a = 10;
+// var b = 20;
+// a++;
+// ++a;
+// console.log(a);
+
+// // typeof
+// console.log(typeof a);
+
+// DataTypes-datatype means type of data which we have.
+    // 1. Primitive
+    // 2. non primitive
+    
+    // 1. Primitive- number,string,boolean(george bully),undefined(empty value),null(empty value)
+        // var a = undefined;
+        // console.log(a);
+
+        // var b = null;
+        // console.log(b);
+    
+    // 2. Non Primitive- array,object   
+        // var states = ["Rajasthan","Harayana","Punjab","UP"];
+        // console.log(states[states.length-1]);
+
+        // Object
+        // var country = {
+        //     1: "India",
+        //     2: "UK",
+        //     3: "Germany",
+        //     4: "Italy"
+        // }
+        // console.log(country["2"]);
+
+        // Ternary Operator
+        // var x = 10;
+        // x>10?console.log("Hello Ved"):console.log("logout")
